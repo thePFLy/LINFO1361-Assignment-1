bba231c1073231811512348dab68a92f88445b6b
Moers Simon <simon.moers@student.uclouvain.be>
2025-03-07 22:06:21 +0100

    haies - bug: invalid format (renvoie None dans verify format)

--- restricted_gardener/restricted_gardener.py
+++ restricted_gardener/restricted_gardener.py
@@ -2,10 +2,29 @@ from pycsp3 import *
 
 
 def solve_restricted_gardener(instruction: int, n:int) -> list[int]:
-    # Put your code here
+    # tableau variables
+    x = VarArray(size=n, dom=range(1, n+1))
 
-    return None
+    satisfy(
+    # constraint 1 (les hauteurs ne doivent pas être les mêmes)
+    [AllDifferent(x)],
+   
 
+    # constraint 2 (vision des haies: si instruction 3 -> 3 haies (ni + ni -) doivent être visibles)
+    Sum(
+            # i (haie) visible si > haies précédentes
+            [x[i] > x[j] for j in range(i)] for i in range(1, n)
+        # 1ere haie tjrs visible car devant
+        ) + 1 == instruction
+    )
+
+    if solve() is SAT:
+        print("SATISFIABLE")
+        return values(x)
+    else:
+        print("UNSATISFIABLE")
+        return None
+    
 def verify_format(solution: list[int], n: int):
     if len(solution) != n:
         print(f"The solution does not contain the right number of cells\n")
